f2c09be0df670865f1cf2db7123cc58f2618d6a7
monashr <mohammednasserkdw@gmail.com>
2025-09-13 21:36:57 +0700

    fix

---
+++ bootstrap/cache/modules.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\MySQL\\Providers\\MySQLServiceProvider',
+    1 => 'Modules\\PostgreSQL\\Providers\\PostgreSQLServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\MySQL\\Providers\\MySQLServiceProvider',
+    1 => 'Modules\\PostgreSQL\\Providers\\PostgreSQLServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
